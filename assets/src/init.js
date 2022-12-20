0432e0144cd24d556d9be1deb23287b55d8a3aaf
AlexanderM1927 <jhon71937@gmail.com>
2022-12-20 10:07:54 -0500

    fly and jump animation

--- assets/src/init.js
+++ assets/src/init.js
@@ -23,8 +23,47 @@ function preload () {
 
     
     this.load.image('bg', '/assets/img/bg.png')
+    this.load.spritesheet('heroe', '/assets/img/heroe.png', {
+        frameWidth: 50,
+        frameHeight: 50
+    })
 }
 
 function create () {
-    this.add.image(400, 300, 'bg')
+    this.add.sprite(400, 300, 'bg')
+    this.player = this.physics.add.sprite(50, 50, 'heroe')
+    this.anims.create({
+        key: 'fly',
+        frames: this.anims.generateFrameNumbers('heroe', {
+            start: 0,
+            end: 1
+        }),
+        frameRate: 7,
+        repeat: -1
+    })
+    this.player.play('fly')
+
+    this.input.keyboard.on('keydown', (event) => {
+        if (event.keyCode === 32) {
+            jump(this)
+        }
+    })
+
+    this.input.on('pointerdown', () => jump(this))
+
+    this.anims.create({
+        key: 'jump',
+        frames: this.anims.generateFrameNumbers('heroe', {
+            start: 2,
+            end: 2
+        }),
+        frameRate: 7,
+        repeat: 1
+    })
+    
+}
+
+function jump (_this) {
+    _this.player.setVelocityY(-200)
+    _this.player.play('jump')
 }
