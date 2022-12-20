65caf5cbba92ac94e78ac1fe9bce308fad7a69d7
AlexanderM1927 <jhon71937@gmail.com>
2022-12-20 10:56:37 -0500

    Added columns to land

--- assets/src/init.js
+++ assets/src/init.js
@@ -1,7 +1,7 @@
 const config = {
     type: Phaser.AUTO,
     width: 800,
-    height: 600,
+    height: 400,
     physics: {
         default: 'arcade',
         arcade: {
@@ -27,20 +27,15 @@ function preload () {
         frameWidth: 50,
         frameHeight: 50
     })
+    this.load.image('pipe0', '/assets/img/pipe0.png')
 }
 
 function create () {
-    this.add.sprite(400, 300, 'bg')
-    this.player = this.physics.add.sprite(50, 50, 'heroe')
-    this.anims.create({
-        key: 'fly',
-        frames: this.anims.generateFrameNumbers('heroe', {
-            start: 0,
-            end: 1
-        }),
-        frameRate: 7,
-        repeat: -1
-    })
+    this.add.sprite(400, 200, 'bg')
+    this.player = this.physics.add.sprite(50, 100, 'heroe')
+    
+    createAnims(this)
+
     this.player.play('fly')
 
     this.input.keyboard.on('keydown', (event) => {
@@ -50,20 +45,55 @@ function create () {
     })
 
     this.input.on('pointerdown', () => jump(this))
+    this.player.on('animationcomplete', animationComplete, this)
 
-    this.anims.create({
+    function animationComplete (animation, frame, sprite) {
+        if (animation.key === 'jump') {
+            this.player.play('fly')
+        }
+    }
+    nuevaColumna(this)
+}
+
+function createAnims (_this) {
+    _this.anims.create({
+        key: 'fly',
+        frames: _this.anims.generateFrameNumbers('heroe', {
+            start: 0,
+            end: 1
+        }),
+        frameRate: 7,
+        repeat: -1
+    })
+    _this.anims.create({
         key: 'jump',
-        frames: this.anims.generateFrameNumbers('heroe', {
-            start: 2,
+        frames: _this.anims.generateFrameNumbers('heroe', {
+            start: 0,
             end: 2
         }),
         frameRate: 7,
         repeat: 1
     })
-    
 }
 
 function jump (_this) {
     _this.player.setVelocityY(-200)
     _this.player.play('jump')
+}
+
+function nuevaColumna (_this) {
+    const columna = _this.physics.add.group()
+    const hueco = Math.floor(Math.random() * 5) + 1
+    for (let i = 0; i < 8; i++) {
+        if ( i !== hueco) {
+            const cubo = columna.create(960, i * 100 + 10, 'pipe0')
+            cubo.body.allowGravity = false
+        }
+    }
+
+    columna.setVelocityX(-200)
+
+    columna.checkWoldBounds = true
+    columna.outOfBoundsKill = true
+    _this.time.delayedCall(1000, _this.nuevaColumna, [], _this)
 }
