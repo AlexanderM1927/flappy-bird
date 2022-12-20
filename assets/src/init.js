0ff556b76dc1f723b117d81b1cad7a6227f2b0be
AlexanderM1927 <jhon71937@gmail.com>
2022-12-20 11:50:51 -0500

    Colision and endGame

--- assets/src/init.js
+++ assets/src/init.js
@@ -1,3 +1,5 @@
+import LoseScene from './LoseScene.js'
+
 const config = {
     type: Phaser.AUTO,
     width: 800,
@@ -10,10 +12,10 @@ const config = {
             }
         }
     },
-    scene: {
+    scene: [{
         preload: preload,
         create: create
-    }
+    }, LoseScene]
 }
 
 const game = new Phaser.Game(config)
@@ -28,23 +30,31 @@ function preload () {
         frameHeight: 50
     })
     this.load.image('pipe0', '/assets/img/pipe0.png')
+    this.load.image('pipeUp0', '/assets/img/pipeUp0.png')
+    this.load.image('pipeDown0', '/assets/img/pipeDown0.png')
+    this.load.image('pipe1', '/assets/img/pipe1.png')
+    this.load.image('pipeUp1', '/assets/img/pipeUp1.png')
+    this.load.image('pipeDown1', '/assets/img/pipeDown1.png')
 }
 
+let _this
+
 function create () {
-    this.add.sprite(400, 200, 'bg')
+    _this = this
+    _this.bg = this.add.tileSprite(400, 200, 800, 400, 'bg').setScrollFactor(0)
     this.player = this.physics.add.sprite(50, 100, 'heroe')
     
-    createAnims(this)
+    createAnims()
 
     this.player.play('fly')
 
     this.input.keyboard.on('keydown', (event) => {
         if (event.keyCode === 32) {
-            jump(this)
+            jump()
         }
     })
 
-    this.input.on('pointerdown', () => jump(this))
+    this.input.on('pointerdown', () => jump())
     this.player.on('animationcomplete', animationComplete, this)
 
     function animationComplete (animation, frame, sprite) {
@@ -52,10 +62,10 @@ function create () {
             this.player.play('fly')
         }
     }
-    nuevaColumna(this)
+    nuevaColumna()
 }
 
-function createAnims (_this) {
+function createAnims () {
     _this.anims.create({
         key: 'fly',
         frames: _this.anims.generateFrameNumbers('heroe', {
@@ -76,17 +86,29 @@ function createAnims (_this) {
     })
 }
 
-function jump (_this) {
+function jump () {
     _this.player.setVelocityY(-200)
     _this.player.play('jump')
 }
 
-function nuevaColumna (_this) {
+function hitColumna() {
+    _this.scene.start('loseScene')
+}
+
+function nuevaColumna () {
     const columna = _this.physics.add.group()
     const hueco = Math.floor(Math.random() * 5) + 1
+    const aleatorio = Math.floor(Math.random() * 2)
     for (let i = 0; i < 8; i++) {
-        if ( i !== hueco) {
-            const cubo = columna.create(960, i * 100 + 10, 'pipe0')
+        if (i !== hueco && i !== hueco + 1 && i !== hueco - 1) {
+            let cubo
+            if (i == hueco - 2) {
+                cubo = columna.create(960, i * 100, 'pipeUp' + aleatorio)
+            } else if (i == hueco + 2) {
+                cubo = columna.create(960, i * 100, 'pipeDown' + aleatorio)
+            } else {
+                cubo = columna.create(960, i * 100, 'pipe' + aleatorio)
+            }
             cubo.body.allowGravity = false
         }
     }
@@ -95,5 +117,6 @@ function nuevaColumna (_this) {
 
     columna.checkWoldBounds = true
     columna.outOfBoundsKill = true
-    _this.time.delayedCall(1000, _this.nuevaColumna, [], _this)
+    _this.time.delayedCall(1000, nuevaColumna, [], _this)
+    _this.physics.add.overlap(_this.player, columna, hitColumna, null, _this)
 }
